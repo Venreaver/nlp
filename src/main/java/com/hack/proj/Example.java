adbadfab362afce620788483c6a94303088b739a
Irina Vasileva <irina_vasileva@epam.com>
2017-12-03 12:53:01 +0300

    update

--- src/main/java/com/hack/proj/Example.java
+++ src/main/java/com/hack/proj/Example.java
@@ -8,7 +8,7 @@ public class Example {
     public static void main(String[] args) {
         SparkSession sparkSession = SparkSession.builder().appName("App").master("local[*]").getOrCreate();
         Dataset<Row> dataset = sparkSession.read().option("header", true)
-                .option("inferSchema", true).csv("src/main/resources/Posts.csv");
+                .option("inferSchema", true).csv("src/main/resources");
         dataset.show(5);
     }
 }
